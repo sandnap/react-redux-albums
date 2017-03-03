f22d6c5c569a5c3ec7537b3d829b9c740172024b
Kirt Henrie <sandnap@gmail.com>
2017-03-03 13:24:23 -0700

    Added title to main page header and implemented material UI table for user list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,15 +10,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Redux Albums</h2>
         </div>
         <div className="App-links">
           <Link to="/">Home</Link>
           <Link to="/users">Users</Link>
         </div>
         <div className="App-content">
-          <Route exact path="/" />
-          <Route exact path="/users" component={Users} />
+          <div className="wrapper">
+            <Route exact path="/" />
+            <Route exact path="/users" component={Users} />
+          </div>
         </div>
       </div>
     );
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,16 +1,48 @@
 import React, { PropTypes } from 'react';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 import './UserList.css';
 
 const UserList = function (props) {
+  const cellClicked = (data) => {
+    console.log(props.userList[data].id);
+  };
+  const address = (addr) => {
+    return (
+      <div className="address">
+        <div>{addr.street}</div>
+        <div>{addr.suite}</div>
+        <div>{addr.city} {addr.zipcode}</div>
+      </div>
+    );
+  };
   const users = () => {
     return props.userList.map((user) => {
-      return <li key={user.id}>{user.name}</li>;
+      return (
+        <TableRow key={user.id} selectable={true}>
+          <TableRowColumn>{user.id}</TableRowColumn>
+          <TableRowColumn>{user.name}</TableRowColumn>
+          <TableRowColumn>{user.email}</TableRowColumn>
+          <TableRowColumn>{address(user.address)}</TableRowColumn>
+        </TableRow>
+      );
     });
   };
   return (
-    <ul className="UserList">
-      {users()}
-    </ul>
+    <div className="UserList">
+      <Table onCellClick={cellClicked}>
+        <TableHeader displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Email</TableHeaderColumn>
+            <TableHeaderColumn>Address</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          {users()}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
